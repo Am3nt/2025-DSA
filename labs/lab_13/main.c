3632defb996e423a235ac91939fa653d4c498598
Am3nt <andrasmihaly13@gmail.com>
2025-05-20 18:18:07 +0300

    Labor 13

--- labs/lab_13/main.c
+++ labs/lab_13/main.c
@@ -1,6 +1,30 @@
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main(void) {
-    printf("Hello, World!\n");
+#include "hash_table.h"
+//#include "../hash_static_uj/headers/hash_table.h"
+
+int main() {
+    HashTable table;
+    createHashArray(&table);
+    display(table);
+    // srand(time(0));
+    if (!freopen("results.txt","r", stdin)) { printf("Error Memory"); return 0; }
+    int n=0;
+    scanf("%i\n",&n);
+    for (int i = 0; i < n; i++) {
+        int key = 0;
+        float temp;
+        char s[7];
+        scanf("%s%f\n",s,&temp);
+        for (int j = 0; j < strlen(s); j++) {
+            key+= s[j];
+        }
+        insert(&table,key,temp, s);
+    }
+    display(table);
+    destroyHashArray(&table);
     return 0;
 }
